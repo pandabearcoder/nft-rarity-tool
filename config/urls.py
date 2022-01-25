814cbc36b791f808902222979831a1f656925ef5
pandabearcoder <vlouie00@gmail.com>
2022-01-25 17:43:48 +0100

    Add view for displaying nft project with it's nft

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from nfts.views import ProjectDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/<int:pk>/', ProjectDetailView.as_view(), name='project_details'),
 ]
--- nfts/views.py
+++ nfts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Project
+
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'nfts/project.html'
+
+    def get_context_data(self, **kwargs):
+        project = self.get_object()
+        nfts = project.nfts.all()
+
+        context = super().get_context_data(**kwargs)
+        context['nfts'] = nfts
+
+        return context
